db88fe7711e5d9b9a4803d1a3c281fdd6a1ce4dc
George Suveti <george.suveti@irian.ro>
2020-05-27 15:23:07 +0300

    cleanup app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-testing'`, () => {
+  it(`should have as title 'Angular testing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-testing');
+    expect(app.title).toEqual('Angular testing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-testing app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Welcome!');
   });
 });
